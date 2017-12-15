4f9101449d0811760f32a1db76ccf466e706612e
Simdi <ezeanietochisimdi@gmail.com>
2017-12-16 00:55:33 +0100

    Updated db.js to show the connection

--- db.js
+++ db.js
@@ -1,2 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.connect(process.env.DB_URL, { useMongoClient: true });
+mongoose.connect(process.env.DB_URL, { useMongoClient: true }, function(port) {
+    console.log('Database connnected successfully');
+});
